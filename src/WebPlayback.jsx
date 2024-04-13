61d564cb28f3eb2513623e26cffbd3b793952126
Melvin Lathara <melvin.lathara@gmail.com>
2024-04-12 17:03:41 -0700

    remove album art

--- src/WebPlayback.jsx
+++ src/WebPlayback.jsx
@@ -110,8 +110,6 @@ function WebPlayback(props) {
                 <div className="container">
                     <div className="main-wrapper">
 
-                        <img src={current_track.album.images[0].url} className="now-playing__cover" alt="" />
-
                         <div className="now-playing__side">
                             <div className="now-playing__name">{current_track.name}</div>
                             <div className="now-playing__artist">{current_track.artists[0].name}</div>
